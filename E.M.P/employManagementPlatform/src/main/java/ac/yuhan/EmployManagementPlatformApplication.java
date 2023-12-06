deee6f4112acc8ae6458e9dd0ca282ffacbb1094
dbwldk <yujiayujia55@yuhan.ac.kr>
2023-12-07 00:47:52 +0900

    프로젝트 파일 추가

---
+++ E.M.P/employManagementPlatform/src/main/java/ac/yuhan/EmployManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package ac.yuhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ E.M.P/employManagementPlatform/src/test/java/ac/yuhan/EmployManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.yuhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
